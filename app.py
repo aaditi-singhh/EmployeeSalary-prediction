c75d0a399c2b6aa8df1bade2d760e1cc00859b0e
aaditi-singhh <aditisingh6841@gmail.com>
2025-07-19 00:43:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ with st.form("prediction_form"):
     occupation = st.selectbox("Occupation", list(encoders['occupation'].keys()))
     relationship = st.selectbox("Relationship", list(encoders['relationship'].keys()))
     race = st.selectbox("Race", list(encoders['race'].keys()))
-    sex = st.radio("Sex", list(encoders['sex'].keys()))
+   
     capital_gain = st.number_input("Capital Gain", min_value=0, value=0)
     capital_loss = st.number_input("Capital Loss", min_value=0, value=0)
     hours_per_week = st.slider("Hours per Week", 1, 100, 40)
@@ -38,7 +38,7 @@ if submit:
             encoders['occupation'][occupation],
             encoders['relationship'][relationship],
             encoders['race'][race],
-            encoders['sex'][sex],
+            
             capital_gain,
             capital_loss,
             hours_per_week,
