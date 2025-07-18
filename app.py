7e4103b2b1e5583ddd6add45c8a7df7959749ecc
aaditi-singhh <aditisingh6841@gmail.com>
2025-07-19 00:48:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ st.title("💼 Employee Salary Prediction App")
 
 with st.form("prediction_form"):
     age = st.slider("Age", 17, 90, 30)
+    fnlwgt = st.number_input("Final Weight (fnlwgt)", min_value=0, value=100000)
     workclass = st.selectbox("Workclass", list(encoders['workclass'].keys()))
     education = st.selectbox("Education", list(encoders['education'].keys()))
     education_num = st.slider("Education Number (Years of Education)", 1, 16, 10)
@@ -19,31 +20,32 @@ with st.form("prediction_form"):
     occupation = st.selectbox("Occupation", list(encoders['occupation'].keys()))
     relationship = st.selectbox("Relationship", list(encoders['relationship'].keys()))
     race = st.selectbox("Race", list(encoders['race'].keys()))
-  capital_gain = st.number_input("Capital Gain", min_value=0, value=0)
+    sex = st.radio("Sex", list(encoders['sex'].keys()))
+    capital_gain = st.number_input("Capital Gain", min_value=0, value=0)
     capital_loss = st.number_input("Capital Loss", min_value=0, value=0)
     hours_per_week = st.slider("Hours per Week", 1, 100, 40)
     native_country = st.selectbox("Native Country", list(encoders['native-country'].keys()))
     
     submit = st.form_submit_button("Predict Salary")
-print(X.columns.tolist()) 
+
 if submit:
     try:
-       input_data = [[
-    age,
-  encoders['workclass'][workclass],
-    encoders['education'][education],
-    education_num,
-    encoders['marital-status'][marital_status],
-    encoders['occupation'][occupation],
-    encoders['relationship'][relationship],
-    encoders['race'][race],
-    encoders['sex'][sex],
-    capital_gain,
-    capital_loss,
-    hours_per_week,
-    encoders['native-country'][native_country]
-]]
-
+        input_data = [[
+            age,
+            fnlwgt,
+            encoders['workclass'][workclass],
+            encoders['education'][education],
+            education_num,
+            encoders['marital-status'][marital_status],
+            encoders['occupation'][occupation],
+            encoders['relationship'][relationship],
+            encoders['race'][race],
+            encoders['sex'][sex],
+            capital_gain,
+            capital_loss,
+            hours_per_week,
+            encoders['native-country'][native_country]
+        ]]
         input_scaled = scaler.transform(input_data)
         prediction = model.predict(input_scaled)[0]
         result = ">50K" if prediction == 1 else "<=50K"
