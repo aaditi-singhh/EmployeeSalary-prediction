a4545816381649b14498004e9f457c1aecb281fc
aaditi-singhh <aditisingh6841@gmail.com>
2025-07-18 23:37:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import joblib
 
 # Load the trained model
-model = joblib.load("train_model.pkl")
+model = joblib.load("knn_salary_model.pkl")
 
 st.set_page_config(page_title="Employee Salary Classification", page_icon="💼", layout="centered")
 
